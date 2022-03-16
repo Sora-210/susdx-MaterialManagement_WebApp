8bf966132b9ffe9bbf75f4d122ac163e0d90f079
Sora-210 <sokominto1231@gmail.com>
2022-03-16 15:55:51 +0000

    [fix] footer追加

--- dx2021_webapp/src/views/pages/Login.vue
+++ dx2021_webapp/src/views/pages/Login.vue
@@ -1,54 +1,62 @@
 <template>
-  <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
-    <CContainer>
-      <CRow class="justify-content-center">
-        <CCol :md="8">
-          <h1>資材管理システム</h1>
-          <CCardGroup>
-            <CCard class="p-4 login-card">
-              <CCardBody>
-                <CForm>
-                  <h1>Login</h1>
-                  <p class="text-medium-emphasis">
-                    管理アカウントでログインしてください
-                  </p>
-                  <CInputGroup class="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon="cil-user" />
-                    </CInputGroupText>
-                    <CFormInput
-                      placeholder="AccountID"
-                      autocomplete="username"
-                    />
-                  </CInputGroup>
-                  <CInputGroup class="mb-4">
-                    <CInputGroupText>
-                      <CIcon icon="cil-lock-locked" />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autocomplete="current-password"
-                    />
-                  </CInputGroup>
-                  <CRow>
-                    <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
-                    </CCol>
-                  </CRow>
-                </CForm>
-              </CCardBody>
-            </CCard>
-          </CCardGroup>
-        </CCol>
-      </CRow>
-    </CContainer>
+  <div>
+    <div class="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow class="justify-content-center">
+          <CCol :md="8">
+            <h1>資材管理システム</h1>
+            <CCardGroup>
+              <CCard class="p-4 login-card">
+                <CCardBody>
+                  <CForm>
+                    <h1>Login</h1>
+                    <p class="text-medium-emphasis">
+                      管理アカウントでログインしてください
+                    </p>
+                    <CInputGroup class="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon="cil-user" />
+                      </CInputGroupText>
+                      <CFormInput
+                        placeholder="AccountID"
+                        autocomplete="username"
+                      />
+                    </CInputGroup>
+                    <CInputGroup class="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon="cil-lock-locked" />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Password"
+                        autocomplete="current-password"
+                      />
+                    </CInputGroup>
+                    <CRow>
+                      <CCol :xs="6">
+                        <CButton color="primary" class="px-4"> Login </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+    <AppFooter />
   </div>
 </template>
 
 <script>
+import AppFooter from '@/components/AppFooter.vue'
+
 export default {
   name: 'Login',
+  components: {
+    AppFooter,
+  },
 }
 </script>
 
