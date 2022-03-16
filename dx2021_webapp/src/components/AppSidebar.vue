daee5bbd4d203c4a516b4da58ee3a4ed20e4c157
Sora-210 <sokominto1231@gmail.com>
2022-03-16 05:24:52 +0000

    [update] header,sidebar作成

--- dx2021_webapp/src/components/AppSidebar.vue
+++ dx2021_webapp/src/components/AppSidebar.vue
@@ -12,22 +12,9 @@
     "
   >
     <CSidebarBrand>
-      <CIcon
-        custom-class-name="sidebar-brand-full"
-        :icon="logoNegative"
-        :height="35"
-      />
-      <CIcon
-        custom-class-name="sidebar-brand-narrow"
-        :icon="sygnet"
-        :height="35"
-      />
+      <img src="@/assets/brand/logo.png" :height="25" />
     </CSidebarBrand>
     <AppSidebarNav />
-    <CSidebarToggler
-      class="d-none d-lg-flex"
-      @click="$store.commit('toggleUnfoldable')"
-    />
   </CSidebar>
 </template>
 
@@ -35,8 +22,6 @@
 import { computed } from 'vue'
 import { useStore } from 'vuex'
 import { AppSidebarNav } from './AppSidebarNav'
-import { logoNegative } from '@/assets/brand/logo-negative'
-import { sygnet } from '@/assets/brand/sygnet'
 export default {
   name: 'AppSidebar',
   components: {
@@ -45,8 +30,6 @@ export default {
   setup() {
     const store = useStore()
     return {
-      logoNegative,
-      sygnet,
       sidebarUnfoldable: computed(() => store.state.sidebarUnfoldable),
       sidebarVisible: computed(() => store.state.sidebarVisible),
     }
