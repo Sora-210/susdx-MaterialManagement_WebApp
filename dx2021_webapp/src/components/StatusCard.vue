dcaab6634c77bc0111a5b3a53f2795518df1e7c6
Sora-210 <sokominto1231@gmail.com>
2022-03-16 15:54:58 +0000

    [add & delete] ステータス表示コンポーネント

---
+++ dx2021_webapp/src/components/StatusCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <CCard :color="StatusData.color">
+    <CCardBody class="ma-6">
+      <CCardTitle class="Text Title mb-2">{{ StatusData.status }}</CCardTitle>
+      <CCardSubtitle class="Text">{{ spaceName }}</CCardSubtitle>
+    </CCardBody>
+  </CCard>
+</template>
+
+<style scoped>
+.Title {
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+.Text {
+  color: #fff;
+}
+</style>
+
+<script>
+export default {
+  name: 'StatusCard',
+  props: {
+    status: {
+      type: String,
+      default: 'many',
+    },
+    spaceName: {
+      type: String,
+      default: 'Camera 0 - No.0',
+    },
+  },
+  setup(props) {
+    const DefaultList = {
+      many: {
+        color: 'primary',
+        status: '多い',
+      },
+      few: {
+        color: 'warning',
+        status: '少ない',
+      },
+      none: {
+        color: 'danger',
+        status: '無し',
+      },
+      cover: {
+        color: 'secondary',
+        status: 'カバー(判定不可)',
+      },
+    }
+    const StatusData = DefaultList[props.status]
+    return {
+      StatusData,
+    }
+  },
+}
+</script>
---
+++ dx2021_webapp/src/components/StatusWidgets.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div v-for="Lists in list" :key="Lists.index">
+      <h3>{{ Lists[0] }}</h3>
+      <CRow class="mb-5" :xs="{ cols: 1 }" :md="{ cols: 3 }" :xl="{ cols: 5 }">
+        <CCol v-for="status in Lists[1]" :key="status.name" class="mb-2">
+          <status-card :status="status.status" :spaceName="status.name" />
+        </CCol>
+      </CRow>
+    </div>
+  </div>
+</template>
+
+<script>
+import StatusCard from '@/components/StatusCard.vue'
+
+export default {
+  name: 'StatusWidgets',
+  components: {
+    StatusCard,
+  },
+  setup() {
+    const list = [
+      [
+        'Camera 1',
+        [
+          {
+            name: 'Camera 1 - No.1',
+            status: 'many',
+          },
+          {
+            name: 'Camera 1 - No.2',
+            status: 'few',
+          },
+          {
+            name: 'Camera 1 - No.3',
+            status: 'cover',
+          },
+        ],
+      ],
+      [
+        'Camera 2',
+        [
+          {
+            name: 'Camera 2 - No.1',
+            status: 'none',
+          },
+          {
+            name: 'Camera 2 - No.2',
+            status: 'many',
+          },
+        ],
+      ],
+      [
+        'Camera 3',
+        [
+          {
+            name: 'Camera 3 - No.1',
+            status: 'few',
+          },
+        ],
+      ],
+      [
+        'Camera 4',
+        [
+          {
+            name: 'Camera 4 - No.1',
+            status: 'none',
+          },
+          {
+            name: 'Camera 4 - No.2',
+            status: 'many',
+          },
+        ],
+      ],
+    ]
+    return {
+      list,
+    }
+  },
+}
+</script>
