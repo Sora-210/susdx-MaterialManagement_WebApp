ed4c1a8515cca5913ef28b72a33df19506687853
Sora-210 <sokominto1231@gmail.com>
2022-03-17 17:38:50 +0000

    [delete] 不要ファイル削除

--- dx2021_webapp/src/main.js
+++ dx2021_webapp/src/main.js
@@ -6,8 +6,6 @@ import store from './store'
 import CoreuiVue from '@coreui/vue'
 import CIcon from '@coreui/icons-vue'
 import { iconsSet as icons } from '@/assets/icons'
-import DocsCallout from '@/components/DocsCallout'
-import DocsExample from '@/components/DocsExample'
 
 import VCalendar from 'v-calendar'
 import 'v-calendar/dist/style.css'
@@ -19,7 +17,5 @@ app.use(CoreuiVue)
 app.use(VCalendar)
 app.provide('icons', icons)
 app.component('CIcon', CIcon)
-app.component('DocsCallout', DocsCallout)
-app.component('DocsExample', DocsExample)
 
 app.mount('#app')
